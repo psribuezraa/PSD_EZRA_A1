f4a2160b0f0e6306d70decef9cce166cab161219
psribuezraa <149931877+psribuezraa@users.noreply.github.com>
2024-03-23 20:42:26 +0700

    infix to postfix.py

--- infix to postfix.py
+++ infix to postfix.py
@@ -1,8 +1,6 @@
-# Function to check if the given character is an operator
 def is_operator(char):
     return char in ['+', '-', '*', '/']
 
-# Function to get the precedence of the operator
 def precedence(operator):
     if operator == '+' or operator == '-':
         return 1
@@ -11,7 +9,6 @@ def precedence(operator):
     else:
         return 0
 
-# Function to convert infix expression to postfix expression
 def infix_postfix(infix):
     postfix = ''
     stack = []
@@ -23,7 +20,7 @@ def infix_postfix(infix):
         elif char == ')':
             while stack and stack[-1] != '(':
                 postfix += stack.pop()
-            stack.pop()  # Pop '('
+            stack.pop() 
         elif is_operator(char):
             while stack and precedence(stack[-1]) >= precedence(char):
                 postfix += stack.pop()
@@ -32,7 +29,6 @@ def infix_postfix(infix):
         postfix += stack.pop()
     return postfix
 
-# Function to evaluate postfix expression
 def evaluate_postfix(postfix):
     stack = []
     for char in postfix:
@@ -51,10 +47,9 @@ def evaluate_postfix(postfix):
                 stack.append(operand1 / operand2)
     return stack.pop()
 
-# Test the functions
 if __name__ == "__main__":
-    infix_expression = input("Enter the infix expression: ")
-    postfix_expression = infix_to_postfix(infix_expression)
-    print("Postfix expression:", postfix_expression)
+    infix_expression = input("Masukan Infix: ")
+    postfix_expression = infix_postfix(infix_expression)
+    print("Postfix:", postfix_expression)
     result = evaluate_postfix(postfix_expression)
-    print("Result:", result)
+    print("Hasil:", result)
